f50a671cb9a48dcb838e6c3598993e1029d7cb55
Jackisace <jackisace@gmail.com>
2023-05-01 15:36:01 +0100

    update

--- runAlgo.py
+++ runAlgo.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import datetime
 
+from IPython import embed
+import pdb
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
@@ -64,56 +67,125 @@ def setPredictions(results):
             h.prediction = float(results[i][0])
             i += 1   
 
+globalwins = 0
+globallosses = 0
+
+money = 0
+wins = 0
+consecutiveLosses = 0
+highestConsecutiveLosses = 0
+money = 0
+wins = 0
+losses = 0
+streak = 0
+highestStreak = 0
+money = 0
+wins = 0
+losses = 0
+streak = 0
+highestStreak = 0
+target = 1
+moneyLost = 0
+mostMoneyLost = 0
+streaks = []
+highestConfidence = 0.0
+
+streaks = []
 def checkPredictions():
-    money = 0
-    wins = 0
-    consecutiveLosses = 0
-    highestConsecutiveLosses = 0
-    money = 0
-    wins = 0
-    losses = 0
-    streak = 0
-    highestStreak = 0
-    money = 0
-    wins = 0
-    losses = 0
-    streak = 0
-    highestStreak = 0
-    target = 1
-    moneyLost = 0
-    mostMoneyLost = 0
-    streaks = []
+    global globalwins, globallosses
+    global money 
+    global wins
+    global consecutiveLosses 
+    global highestConsecutiveLosses 
+    global money 
+    global wins 
+    global losses 
+    global streak 
+    global highestStreak 
+    global money 
+    global wins 
+    global losses 
+    global streak 
+    global highestStreak 
+    global target 
+    global moneyLost 
+    global mostMoneyLost 
+    global streaks 
+    global highestConfidence 
     
     streaks = []
+#    pdb.set_trace()
     for r in tomorrowsRaces:
-        highest = None
-        highestPrediction = 0.0
-        for h in r.horses:
-            if h.prediction > highestPrediction:
-                highest = h
-                highestPrediction = h.prediction
-        
-        if highest.position == 1:
-            wins += 1
-            money = target
-            target += 1
-            if moneyLost > mostMoneyLost:
-                mostMoneyLost = moneyLost
-            moneyLost = 0
-        else:
-            losses += 1
-            diff = target - money
-            diffCost = diff / highest.price
-            moneyLost += diffCost
-            money -= diffCost
+        try:
+            highest = None
+            highestPrediction = 0.0
+            secondHighest = None
+            for h in r.horses:
+                if h.prediction > highestPrediction:
+                    highest = h
+                    highestPrediction = h.prediction
+                    if highestPrediction > highestConfidence:
+                        highestConfidence = highestPrediction
+            highestPrediction = 0.0
+            for h in r.horses:
+                if h is highest:
+#                    print("highest found")
+                    continue
+                if h.prediction > highestPrediction:
+                    secondHighest = h
+                    highestPrediction = h.prediction
+                    if highestPrediction > highestConfidence:
+                        highestConfidence = highestPrediction
+            
 
             
-        print(wins, consecutiveLosses, money)
+            if not highest:
+                continue
+            if highest.prediction < 0.5:
+                continue
+            if highest.price > 1:
+                continue
+            
+            if highest.position == 1:
+                wins += 1
+                globalwins += 1
+                money = target
+                target += 1
+                if moneyLost > mostMoneyLost:
+                    mostMoneyLost = moneyLost
+                moneyLost = 0
+                consecutiveLosses = 0
+            else:
+                losses += 1
+                globallosses += 1
+                consecutiveLosses += 1
+                diff = float(target - money)
+                diffCost = diff / highest.price
+                moneyLost += diffCost
+                money -= diffCost
+            print("wins:{}    losses:{}    consecutive losses:{}    money:{}    diff:{}".format(wins, losses, consecutiveLosses, money, (highest.prediction - secondHighest.prediction)))
+        except:
+            pass
+
+    print("Highest confidence:", highestConfidence)
     print("highest consecutive losses:", highestConsecutiveLosses)
     print("most money lost:", mostMoneyLost)
+    print("global wins: {}\tglobal losses: {}".format(globalwins, globallosses))
 
 
+def checkNewResults(results):
+    best = 0
+    betOn = None
 
+    for i in range(0, len(results)):
+        if results[i] > best:
+            best = results[i]
+            betOn = i
+
+    if y_pred[betOn][0] == 1:
+        print("winner")
+    else:
+        print("loser")
             
 
 def checkResults(results):
@@ -248,34 +320,36 @@ for month in range(6,8):
         y_pred = y[tomorrowsLength:]
 
 
-        try:
+        #try:
 
-            ann = tf.keras.models.Sequential()
-            ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
-            ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
-            ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
-            ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
-            ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
-            ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
-            ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
-            ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
-            ann.add(tf.keras.layers.Dense(units=1, activation="sigmoid"))
-            ann.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-            ann.fit(x_train, y_train, batch_size = 500, epochs = 10)
-
-
-            results = ann.predict(x_pred)
+        ann = tf.keras.models.Sequential()
+        ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
+        ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
+        ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
+        ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
+        ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
+        ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
+        ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
+        ann.add(tf.keras.layers.Dense(units=40, activation="relu"))
+        ann.add(tf.keras.layers.Dense(units=1, activation="sigmoid"))
+        ann.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+        ann.fit(x_train, y_train, batch_size = 500, epochs = 20)
 
 
+        results = ann.predict(x_pred)
 
-            money = checkResults(results)
 
-            allMoney.append(money)
 
-            setPredictions(results)
-            checkPredictions()
-        except:
-            pass
+        #money = checkResults(results)
+        #checkNewResults(results)
+
+        #embed()
+        #allMoney.append(money)
+
+        setPredictions(results)
+        checkPredictions()
+        #except:
+        #    pass
 
 
 zpone = 0
